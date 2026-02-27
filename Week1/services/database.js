f90da942daaf69e7f688e8b10ed6af8625dcd68a
kidfles <166709765+kidfles@users.noreply.github.com>
2026-02-27 16:56:00 +0100

    1.6 Omgevingsvariable

--- Week1/services/database.js
+++ Week1/services/database.js
@@ -1,22 +1,15 @@
+require("dotenv").config();
+
 const { MongoClient } = require("mongodb");
 
-
-
-const uri = "mongodb://localhost:27017";
-
-
+const uri = process.env.MONGO_URL || "mongodb://localhost:27017";
+const dbName = process.env.DB_NAME || "mydb";
 
 const client = new MongoClient(uri);
 
-
-
-const db = client.db("mydb");
-
-
+const db = client.db(dbName);
 
 module.exports = {
-
     db: db,
     client: client
-
 };
